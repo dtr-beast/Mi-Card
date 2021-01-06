ae3c25ea91970bc4ce40dee408ce5d7ce764724b
dtr-beast <55450440+dtr-beast@users.noreply.github.com>
2021-01-06 15:41:46 +0530

    Completed 6th Module, Mi-Card App. 💪🏿💪🏿 Used Card and ListTile Widget for adding phone number and e-mail 🤑 Also centered the whole App for Web Also ✔

--- lib/main.dart
+++ lib/main.dart
@@ -9,83 +9,98 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green[300],
-        body: Column(
-          children: <Widget>[
-            CircleAvatar(
-                radius: 60.00, backgroundImage: AssetImage('images/MyPic.jpg')),
-            Text(
-              'Aditya Sharma',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontFamily: 'Pacifico',
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Source Sans Pro',
-                color: Colors.yellow[500],
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    size: 35.0,
-                    color: Colors.teal,
+        backgroundColor: Colors.teal[800],
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 60.00,
+                  backgroundImage: AssetImage('images/MyPic.jpg'),
+                ),
+                Text(
+                  'Aditya Sharma',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
-                  SizedBox(
-                    width: 10.0,
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.yellow[500],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text(
-                    '+91 845-975-0387',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontFamily: 'Pacifico',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.teal[900],
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    thickness: 1.0,
+                     color: Colors.white,
+                  ),
+                ),
+                Container(
+                  width: 400.0,
+                  height: 100.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.phone,
+                          size: 35.0,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          '+91 845-456-0387',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontFamily: 'Pacifico',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.teal[900],
+                          ),
+                        ),
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.mail,
-                    size: 35.0,
-                    color: Colors.teal,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'mc5torkpro@gmail.com',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontFamily: 'Pacifico',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.teal[900],
+                ),
+                Container(
+                  width: 400.0,
+                  height: 100.0,
+                  child: Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.mail,
+                          size: 35.0,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          'mc5torkpro@gmail.com',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontFamily: 'Pacifico',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.teal[900],
+                          ),
+                        ),
+                      ),
                     ),
-                  )
-                ],
-              ),
-            )
-          ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
